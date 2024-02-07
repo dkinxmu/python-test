d46c21d45817333dd4d187222072f0eed18a9b70
dkinxmu <93137231+dkinxmu@users.noreply.github.com>
2024-02-07 04:02:33 +0000

    for&&break

---
+++ break.py
@@ -0,0 +1,5 @@
+while True:
+    string = input('Plz type sth:')
+    if string == 'quit':
+        break
+    print('length is',len(string))
---
+++ for.py
@@ -0,0 +1,4 @@
+from tqdm import tqdm
+i = 6
+for k in tqdm(range(1,5)):
+    print(k)
